62b406dcdc19bcb805482f69c3d24f8075791b96
Daniel Hernán Rico Freitas <danielhernanricofreitas@gmail.com>
2023-12-03 12:47:00 +0100

    01-2 take 2

--- 01-2.py
+++ 01-2.py
@@ -5,13 +5,14 @@ Author: Daniel Hernán Rico Freitas
 github: https://github.com/dh-rico-freitas
 """
 
+from argparse import ArgumentParser
 from collections import defaultdict
 
 PUZZLE = "01-2"
 INPUTS_PATH = "inputs/01-1.txt"
 
 
-def main():
+def main(get_number):
     with open(INPUTS_PATH) as f:
         numbers = (get_number(l) for l in f)
         print(sum(numbers))
@@ -41,11 +42,12 @@ NUMBERS = {
 
 SREBMUN = {key[::-1]: value for key, value in NUMBERS.items()}
 
-def generate_token_table(tokens):
 
+def generate_token_table(tokens):
     def create_test(target):
         def f(string):
-            return target == string[:len(target)]
+            return target == string[: len(target)]
+
         return f
 
     def process_test_list(l):
@@ -54,14 +56,16 @@ def generate_token_table(tokens):
                 if test(s):
                     return result
             return None
+
         return f
 
     table = defaultdict(list)
 
     for key, value in tokens.items():
         table[key[0]].append((create_test(key), value))
-    
+
     return {key: process_test_list(value) for key, value in table.items()}
+
 
 NUMBERS_TEST = generate_token_table(NUMBERS)
 SREBMUN_TEST = generate_token_table(SREBMUN)
@@ -70,9 +74,9 @@ SREBMUN_TEST = generate_token_table(SREBMUN)
 def get_number(string):
     for i, char in enumerate(string):
         if (
-                char in NUMBERS_TEST
-                and (first := NUMBERS_TEST[char](string[i:])) is not None
-                ):
+            char in NUMBERS_TEST
+            and (first := NUMBERS_TEST[char](string[i:])) is not None
+        ):
             break
     else:
         raise ValueError("Didn't find any number")
@@ -80,9 +84,9 @@ def get_number(string):
     reversed_string = string[::-1]
     for i, char in enumerate(reversed_string):
         if (
-                char in SREBMUN_TEST
-                and (last := SREBMUN_TEST[char](reversed_string[i:])) is not None
-                ):
+            char in SREBMUN_TEST
+            and (last := SREBMUN_TEST[char](reversed_string[i:])) is not None
+        ):
             break
     else:
         raise ValueError(f"Didn't find any number in reverse direction. {string=}")
@@ -90,5 +94,40 @@ def get_number(string):
     return first * 10 + last
 
 
+def linear_search(string, reference):
+    for i in range(len(string)):
+        substring = string[i:]
+        for key, value in reference.items():
+            if substring.startswith(key):
+                return value
+    else:
+        raise ValueError("Didn't find any number")
+
+
+def get_number_simple(string):
+    first = linear_search(string, NUMBERS)
+    last = linear_search(string[::-1], SREBMUN)
+    return first * 10 + last
+
+
 if __name__ == "__main__":
-    main()
+    import argparse
+    import time
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--timed", action="store_true")
+    parser.add_argument("-s", "--simple", action="store_true")
+    args = parser.parse_args()
+
+    if args.timed:
+        start_time = time.perf_counter()
+
+    processor = get_number
+    if args.simple:
+        processor = get_number_simple
+
+    main(processor)
+
+    if args.timed:
+        end_time = time.perf_counter()
+        print(f"Execution time= {(end_time - start_time) * 1000} ms")
--- README.md
+++ README.md
@@ -28,23 +28,50 @@ Only comment, read the problem to the end instead of half guessing the question.
 
 WHO let the F** JUNNIOR do the job?
 
-Ideas:
-* Replace text nubmers by digits and use the same get_number function
-  used in puzzle 1. 
+### Ideas:
+1- Replace text nubmers by digits and use the same get_number function
+   used in puzzle 1. 
+ 
+   Lot of unnecesary work. Risk of some overlapping numbers messing the
+   conversion (eighThree)
 
-  Lot of unnecesary work. Risk of some overlapping numbers messing the
-  conversion (eighThree)
+2- Get the index of all the starting possition of number words and individual
+   digits. 
 
-* Get the index of all the starting possition of number words and individual
-  digits. 
+   Also lot of repeated work.
 
-  Also lot of repeated work.
+3- Make a function that walk the string looking for any of the possible first
+   characters.
+   Check if the substring starting in this possition is a number.
+   On succes return the number converted to int, on failure continue.
 
-* Make a function that walk the string looking for any of the possible first
-  characters.
-  Check if the substring starting in this possition is a number.
-  On succes return the number converted to int, on failure continue.
+   I could compile a table of firsts, seconds, thirds ... characters, 
+   use that instead of checking for the string, but I think this is a good
+   middle ground.
 
-* Go to read 
+4- Go to read Skiena. 
 
-* All from above using regex.
+5- 1-4 from above using regex.
+
+### Let's try idea 3.
+
+I have to handcraft the NUMBERS dict, but from there all is automatic,
+and I could modify this for othere languages. Is there a little issue
+if in some language one number is a prefix of other.
+
+I like the flexibility, but dislike the extra complexity of making a 
+dict of first letters. I'd like to test if this make a dent on performance.
+
+### Take 2. 
+Let's try idea 2 and see if it performance is too worst.
+
+Add some argparse boilerplate to choose what function use
+time the results. 
+
+Idea 3. is around 10x faster than 2., so it wasn't a bad idea to implement
+it in that way. But the second code is far cleaner. Maybe I should try
+to fix that.
+
+
+
+
